7b469ab435a6ece0a64d86666c7dbbb4b924bcde
milindkusahu <milindsahu2000@gmail.com>
2024-10-23 00:55:57 +0530

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Sidebar from "./components/Sidebar/Sidebar";
+// import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
   return (
     <>
-      <Sidebar />
+      {/* <Sidebar /> */}
     </>
   );
 };
